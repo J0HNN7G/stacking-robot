adad04b1be7d9c9de25097fd7636f4a2ef4d8617
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2021-05-02 20:06:32 +0100

    Added documentation to Component and Motor class

--- components/actuators/motor.py
+++ components/actuators/motor.py
@@ -5,30 +5,72 @@ import component
 import RPi.GPIO as GPIO
 
 class Motor(component.Component):
+    """A class for controlling the wheel motors of the robot."""
 
+    # Standard frequency (Hz) of PWM instance controlling motor engine.
     STD_FREQ = 1000
 
     @property
     def pwm(self):
+        # type: None -> GPIO.PWM
+        """
+        Get the PWM instance controlling the motor engine.
+
+        :return: PWM instance controlling the motor engine
+        """
         return self._pwm
 
     @property
     def radius(self):
+        # type: None -> bool
+        """
+        Get the radius of the motor's wheel.
+
+        :return: the radius of the motor's wheel in millimetres
+        """
         return self._radius
 
     @property
     def enginePin(self):
+        # type: None -> int
+        """
+        Get the GPIO number of pin controlling the motor engine.
+
+        :return: GPIO number of pin controlling the motor engine
+        """
         return self._enginePin
 
     @property
     def backwardPin(self):
+        # type: None -> int
+        """
+        Get the GPIO number of pin controlling the backward motion.
+
+        :return: GPIO number of pin controlling the backward motion
+        """
         return self._backwardPin
 
     @property
     def forwardPin(self):
+        # type: None -> int
+        """
+        Get the GPIO number of pin controlling the forward motion.
+
+        :return: GPIO number of pin controlling the forward motion
+        """
         return self._forwardPin
 
     def __init__(self, radius, enginePin, backwardPin, forwardPin):
+        # type: (float, int, int, int) -> None
+        """
+        Initialise a motor with the radius of the wheel it spins and the
+        GPIO numbered pins that control it.
+
+        :param radius: the radius of the motor's wheel in millimetres
+        :param enginePin: GPIO number of pin controlling motor engine
+        :param backwardPin: GPIO number of pin controlling backward motion
+        :param forwardPin: GPIO number of pin controlling forward motion
+        """
         status(False)
         radius(radius)
         enginePin(enginePin)
@@ -36,7 +78,12 @@ class Motor(component.Component):
         forwardPin(forwardPin)
 
     def setup(self):
+        # type: None -> None
+        """
+        Setup the motor for use.
+        """
         GPIO.setwarnings(False)
+        # Set to GPIO numbering
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(enginePin(), GPIO.OUT)
     	GPIO.setup(backwardPin(), GPIO.OUT)
@@ -44,41 +91,87 @@ class Motor(component.Component):
 
         stop()
         try:
+            # Create PWM instance and output
             pwm(GPIO.PWM(enginePin(), STD_FREQ))
+            status(True)
         except Exception as e:
             print(e)
             cleanup()
 
     def cleanup(self):
+        # type: None -> None
+        """
+        Cleanup the motor.
+        """
         stop()
         GPIO.cleanup([enginePin(), backwardPin(), forwardPin()])
+        status(False)
 
     def stop(self):
+        # type: None -> None
+        """
+        Stop the motor.
+        """
         GPIO.output(enginePin(), GPIO.LOW)
         GPIO.output(backwardPin(), GPIO.LOW)
         GPIO.output(forwardPin(), GPIO.LOW)
 
     @pwm.setter
     def pwm(self, val):
-        error.checkType(val, GPIO.PWM)
+        # type: (GPIO.PWM) -> None
+        """
+        Set the PWM instance controlling the motor engine.
+
+        :param val: the PWM instance controlling the motor engine.
+        :raise TypeError: if val is not 'GPIO.PWM'
+        """
+        error.checkType(val, GPIO.PWM, 'pwm must be of type GPIO.PWM!')
         self._pwm = val
 
     @radius.setter
     def radius(self, val):
+        # type: (float) -> None
+        """
+        Set the radius of the motor's wheel.
+
+        :param val: the radius of the motor's wheel in millimetres
+        :raise TypeError: if val is not a float
+        """
         error.checkType(val, float, 'radius must be a float!')
         self._radius = val
 
     @enginePin.setter
     def enginePin(self, val):
+        # type: (int) -> None
+        """
+        Set the GPIO number of pin controlling motor engine.
+
+        :param val: GPIO number of pin controlling motor engine
+        :raise TypeError: if val is not an int
+        """
         error.checkType(val, float, 'enginePin must be an int!')
         self._enginePin = val
 
     @backwardPin.setter
     def backwardPin(self, val):
+        # type: (int) -> None
+        """
+        Set the GPIO number of pin controlling backward motion.
+
+        :param val: GPIO number of pin controlling backward motion
+        :raise TypeError: if val is not an int
+        """
         error.checkType(val, float, 'backwardPin must be a float!')
         self._backwardPin = val
 
     @forwardPin.setter
     def forwardPin(self, val):
+        # type: (int) -> None
+        """
+        Set the GPIO number of pin controlling forward motion
+
+        :param val: GPIO number of pin controlling forward motion
+        :raise TypeError: if val is not an int
+        """
         error.checkType(val, float, 'forwardPin must be a float!')
         self._forwardPin = val
--- components/component.py
+++ components/component.py
@@ -4,24 +4,46 @@ import error
 from abc import ABC, abstractmethod
 
 class Component(ABC):
+    """
+    An abstract class for the hardware components of the robot.
+    """
 
     @property
     def status(self):
+        # type: None -> bool
+        """
+        Get the status of the component (on/off).
+
+        :return: true if the component is on, false if if it is off
+        """
         return self._status
 
     @abstractmethod
     def __init__(self):
+        # type: (...) -> (...)
+        """Initialise the component."""
         pass
 
     @abstractmethod
     def setup(self):
+        # type: (...) -> (...)
+        """Setup the component."""
         pass
 
     @abstractmethod
     def cleanup(self):
+        # type: (...) -> (...)
+        """Cleanup the component."""
         pass
 
     @status.setter
     def status(self, val):
+        # type: bool -> None
+        """
+        Set the status of the component (on/off).
+
+        :param val: true if component is on, false if it is off
+        :raise TypeError: if val is not a bool
+        """
         error.checkType(val, bool, 'status must be a boolean!')
         self._status = val
