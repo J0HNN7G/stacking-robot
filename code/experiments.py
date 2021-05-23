c70f17d49f63a0d4d25cd730d38e445e39e8d3a7
Jonathan Gustafsson Frennert <jonathan.frennert@gmail.com>
2021-05-23 21:48:12 +0100

    Fixed GPIO pins

--- code/experiments.py
+++ code/experiments.py
@@ -4,6 +4,6 @@ from move import Move
 from motor import Motor
 
 leftMotor = Motor(4, 14, 15)
-rightMotor = Motor(17, 27, 18)
+rightMotor = Motor(17, 18, 27)
 
 robot = Move(Move.MAX_DC, leftMotor, rightMotor)
