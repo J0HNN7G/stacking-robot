b6b7e06fb5e8ef5cf94c502e5f4359ee18fd964d
Jonathan Frennert <jonathan.frennert@gmail.com>
2021-05-28 14:01:22 +0200

    Fixed left and right motor gpio indexing

--- code/experiments.py
+++ code/experiments.py
@@ -3,7 +3,7 @@
 from move import Move
 from motor import Motor
 
-leftMotor = Motor(4, 14, 15)
-rightMotor = Motor(17, 18, 27)
+leftMotor = Motor(17, 18, 27)
+rightMotor = Motor(4, 14, 15)
 
 robot = Move(99.5, leftMotor, rightMotor)
