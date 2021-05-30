7efd44574db91eb869f8e026098a34b35e124db7
Jonathan Frennert <jonathan.frennert@gmail.com>
2021-05-30 08:45:07 +0200

    first version of Arm class

--- code/arm.py
+++ code/arm.py
@@ -2,11 +2,13 @@
 
 
 import error
+from adafruit_servokit import ServoKit
 
 
-class Arm:
+class Arm(Component):
     """A class for controlling the arm of the robot."""
 
+    INIT_ANGLE = 90
 
     def __init__(self, shoulderPin, elbowPin, wristPin, grabberPin):
         error.checkPCA9685(shoulderPin)
@@ -14,7 +16,25 @@ class Arm:
         error.checkPCA9685(wristPin)
         error.checkPCA9685(grabberPin)
 
-        self.shoulderPin = shoulderPin
-        self.elbowPin = elbowPin
-        self.wristPin = wristPin
-        self.grabberPin = grabberPin
+        set.status = False
+        self.kit = ServoKit(channels=16)
+        self.shoulder = self.kit.servo[shoulderPin]
+        self.elbow = self.kit.servo[elbowPin]
+        self.wrist = self.kit.servo[wristPin]
+        self.grabber = self.kit.servo[grabberPin]
+
+
+    def setup(self):
+        self.shoulder.angle = self.INIT_ANGLE
+        self.elbow.angle = self.INIT_ANGLE
+        self.wrist.angle = self.INIT_ANGLE
+        self.grabber.angle = self.INIT_ANGLE
+        self.status = True
+
+
+    def cleanup(self):
+        self.shoulder.angle = self.INIT_ANGLE
+        self.elbow.angle = self.INIT_ANGLE
+        self.wrist.angle = self.INIT_ANGLE
+        self.grabber.angle = self.INIT_ANGLE
+        self.status = False
--- code/experiments.py
+++ code/experiments.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from move import Move
+from arm import Arm
+from body import Body
 from motor import Motor
 
 leftMotor = Motor(17, 18, 27)
 rightMotor = Motor(4, 14, 15)
 
-robot = Body(MOVE.MAX_MOTOR_DC, leftMotor, rightMotor)
+body = Body(MOVE.MAX_MOTOR_DC, leftMotor, rightMotor)
+arm = Arm(12,13,14,15)
