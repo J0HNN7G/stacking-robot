7057a49e7b1113514426c1628c9903147f8747a2
Alexandra <apurcarea@ymail.com>
2021-08-27 11:57:05 +0300

    finished execution function

--- code/control.py
+++ code/control.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import error
+import time
+import math
 
 class Control():
+
+    ANGLE_INC = 5
+    SLEEPY_TIME = 0.1
+
     def __init__(self, arm):
         self.arm = arm
         self.shoulderAngle = None
@@ -11,7 +17,42 @@ class Control():
         self.grabberAngle = None
 
     def execute(self):
-        
+        while not((self.shoulderAngle is None) and (self.elbowAngle is None) and
+                  (self.wristAngle is None) and (self.grabberAngle is None)):
+
+            if self.shoulderAngle is not None:
+                angleDiff = self.shoulderAngle - self.arm.shoulder()
+                change = min(abs(angleDiff), self.ANGLE_INC)*math.copysign(1, angleDiff)
+                self.arm.shoulder(self.arm.shoulder() + change)
+
+                if self.arm.shoulder() == self.shoulderAngle:
+                    self.shoulderAngle = None
+
+            if self.elbowAngle is not None:
+                angleDiff = self.elbowAngle - self.arm.elbow()
+                change = min(abs(angleDiff), self.ANGLE_INC)*math.copysign(1, angleDiff)
+                self.arm.elbow(self.arm.elbow() + change)
+
+                if self.arm.elbow() == self.elbowAngle:
+                    self.elbowAngle = None
+
+            if self.wristAngle is not None:
+                angleDiff = self.wristAngle - self.arm.wrist()
+                change = min(abs(angleDiff), self.ANGLE_INC)*math.copysign(1, angleDiff)
+                self.arm.wrist(self.arm.wrist() + change)
+
+                if self.arm.wrist() == self.wristAngle:
+                    self.wristAngle = None
+
+            if self.grabberAngle is not None:
+                angleDiff = self.grabberAngle - self.arm.grabber()
+                change = min(abs(angleDiff), self.ANGLE_INC)*math.copysign(1, angleDiff)
+                self.arm.grabber(self.arm.grabber() + change)
+
+                if self.arm.grabber() == self.grabberAngle:
+                    self.grabberAngle = None
+
+            time.sleep(self.SLEEPY_TIME)
 
     def shoulder(self, angle):
         error.checkInRange(angle, self.arm.SHOULDER_MIN_DOM, self.arm.SHOULDER_MAX_DOM)
