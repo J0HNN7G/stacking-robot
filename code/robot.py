409163b8efcb6ce0cacfe7b02c517db2668e96e4
Jonathan Frennert <jonathan.frennert@gmail.com>
2021-09-14 09:58:18 +0200

    implemented close find

--- code/robot.py
+++ code/robot.py
@@ -2,7 +2,7 @@
 
 import error
 from component import Component
-from inverse_kinematics import calcAngles
+import inverse_kinematics as ik
 from arm import Arm
 from head import Head
 import head
@@ -38,17 +38,33 @@ class Robot(Component):
     # Vertical center of camera images in pixels.
     CENTER_IMG_Y = Head.IMG_HEIGHT // 2
 
-    # The divisor of image width or height in pixels to determine if object is within center view
-    THRESH_DIV = 5
+    # The divisor of image width or height in pixels to determine
+    # if object is within center view for faraway find.
+    FAR_THRESH_DIV = 5
 
-    # Duration of movement per camera frame in entity search.
-    MOVE_TIME = 0.2
+    # Duration of movement per camera frame in faraway entity search.
+    FAR_MOVE_TIME = 0.2
 
-    # Angle change per camera frame if object is outside of vertical center view
-    VIEW_DIFF = 5
-    
-    # Readjustment period for camera after search movement.
-    REF_TIME = 0.25
+    # Angle change per camera frame if object is outside of vertical
+    # center view for faraway search.
+    FAR_VIEW_DIFF = 5
+
+    # Readjustment period for camera after search movement for faraway search.
+    FAR_REF_TIME = 0.25
+
+    # The divisor of image width or height in pixels to determine
+    # if object is within center view for faraway find.
+    CLOSE_THRESH_DIV = 7
+
+    # Duration of movement per camera frame in faraway entity search.
+    CLOSE_MOVE_TIME = 0.05
+
+    # Angle change per camera frame if object is outside of vertical
+    # center view for faraway search.
+    CLOSE_VIEW_DIFF = 3
+
+    # Readjustment period for camera after search movement for faraway search.
+    CLOSE_REF_TIME = 0.5
 
 
     def __init__(self, head, body, arm):
@@ -97,7 +113,7 @@ class Robot(Component):
 
         result = False
         objPos = self.head.objPos()
-        angles = calcAngles(objPos)
+        angles = ik.calcAngles(objPos)
 
         if angles is not None:
             shoulderAngle, elbowAngle = angles
@@ -128,10 +144,12 @@ class Robot(Component):
         return result
 
 
-    def find(self, entity, timeLim):
+    def farFind(self, entity, timeLim):
         error.checkType(entity, Entity, 'entity', 'Entity')
         if entity == Entity.FLOOR:
             return True
+
+        self.head.view = 0
 
         with PiCamera() as camera:
             camera.resolution = (Head.IMG_WIDTH, Head.IMG_HEIGHT)
@@ -155,15 +173,15 @@ class Robot(Component):
                         inView = True
 
                         # Too high
-                        if (objProp['y'] > self.CENTER_IMG_Y + (Head.IMG_HEIGHT // self.THRESH_DIV)) and (self.head.view != 0):
-                            self.head.view = self.head.view - min(self.VIEW_DIFF, self.head.view)
-                            print('Lowering')
+                        if (objProp['y'] > self.CENTER_IMG_Y + (Head.IMG_HEIGHT // self.FAR_THRESH_DIV)) and (self.head.view != 0):
+                            self.head.view = self.head.view - min(self.FAR_VIEW_DIFF, self.head.view)
+                            print('down')
 
                         # Too low
-                        elif objProp['y'] < self.CENTER_IMG_Y - (Head.IMG_HEIGHT // self.THRESH_DIV):
-                            diff = min(self.VIEW_DIFF, Head.VIEW_RNG - self.head.view)
+                        elif objProp['y'] < self.CENTER_IMG_Y - (Head.IMG_HEIGHT // self.FAR_THRESH_DIV):
+                            diff = min(self.FAR_VIEW_DIFF, Head.VIEW_RNG - self.head.view)
                             self.head.view = self.head.view + diff
-                            print('rising')
+                            print('up')
 
                             # If we get closer, the object will get out of view
                             if math.isclose(self.head.view, Head.VIEW_RNG, abs_tol=1):
@@ -171,18 +189,18 @@ class Robot(Component):
 
 
                         # Too left
-                        if objProp['x'] > self.CENTER_IMG_X + (Head.IMG_WIDTH // self.THRESH_DIV):
-                            self.body.move(self.MOVE_TIME, Direction.RIGHT)
+                        if objProp['x'] > self.CENTER_IMG_X + (Head.IMG_WIDTH // self.FAR_THRESH_DIV):
+                            self.body.move(self.FAR_MOVE_TIME, Direction.RIGHT)
                             print('left')
 
                         # Too right
-                        elif objProp['x'] < self.CENTER_IMG_X - (Head.IMG_WIDTH // self.THRESH_DIV):
-                            self.body.move(self.MOVE_TIME, Direction.LEFT)
+                        elif objProp['x'] < self.CENTER_IMG_X - (Head.IMG_WIDTH // self.FAR_THRESH_DIV):
+                            self.body.move(self.FAR_MOVE_TIME, Direction.LEFT)
                             print('right')
 
                         # Perfect horizontal view
                         else:
-                            self.body.move(self.MOVE_TIME, Direction.FORWARD)
+                            self.body.move(self.FAR_MOVE_TIME, Direction.FORWARD)
                             print('forward')
 
 
@@ -195,7 +213,7 @@ class Robot(Component):
                             print('lost it')
                         elif (time.time() - searchStart > timeLim):
                             return False
-                        self.body.move(self.MOVE_TIME, Direction.LEFT)
+                        self.body.move(self.FAR_MOVE_TIME, Direction.LEFT)
 
                     else:
                         return True
@@ -207,8 +225,80 @@ class Robot(Component):
                         # start searching again
                         inView = False
                         searchStart = time.time()
-                    self.body.move(self.MOVE_TIME, Direction.LEFT)
-                
+                    self.body.move(self.FAR_MOVE_TIME, Direction.LEFT)
+
                 # readjust the camera
-                time.sleep(self.REF_TIME)
+                time.sleep(self.FAR_REF_TIME)
+                rawCapture.truncate(0)
+
+
+    def closeFind(self, entity, timeLim):
+        error.checkType(entity, Entity, 'entity', 'Entity')
+        if entity == Entity.FLOOR:
+            return True
+
+        with PiCamera() as camera:
+            camera.resolution = (Head.IMG_WIDTH, Head.IMG_HEIGHT)
+            camera.framerate = Head.FRAMERATE
+            rawCapture = PiRGBArray(camera, size=(Head.IMG_WIDTH, Head.IMG_HEIGHT))
+
+            for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+                image = frame.array
+                hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                colMask = head.colourMask(hsv, entity)
+                objProp = head.findObjProp(colMask)
+
+                if objProp:
+                    print(objProp)
+                    if objProp['area'] < self.MIN_AREA:
+                        print('way too small')
+                        return False
+
+
+                        # Too high
+                    if (objProp['y'] > self.CENTER_IMG_Y + (Head.IMG_HEIGHT // self.CLOSE_THRESH_DIV)) and (self.head.view != 0):
+                        self.head.view = self.head.view - min(self.CLOSE_VIEW_DIFF, self.head.view)
+                        print('down')
+                        # Too low
+                    elif objProp['y'] < self.CENTER_IMG_Y - (Head.IMG_HEIGHT // self.CLOSE_THRESH_DIV):
+                        diff = min(self.CLOSE_VIEW_DIFF, Head.VIEW_RNG - self.head.view)
+                        self.head.view = self.head.view + diff
+                        print('up')
+                        # If we get closer, the object will get out of view
+                        if math.isclose(self.head.view, Head.VIEW_RNG, abs_tol=1):
+                            return True
+
+                    # Too left
+                    if objProp['x'] > self.CENTER_IMG_X + (Head.IMG_WIDTH // self.CLOSE_THRESH_DIV):
+                        self.body.move(self.CLOSE_MOVE_TIME, Direction.RIGHT)
+                        print('left')
+                    # Too right
+                    elif objProp['x'] < self.CENTER_IMG_X - (Head.IMG_WIDTH // self.CLOSE_THRESH_DIV):
+                        self.body.move(self.CLOSE_MOVE_TIME, Direction.LEFT)
+                        print('right')
+                    else:
+                        objPos = self.head.objPos()
+                        # distance between shoulder axis and target
+                        tDist = round( math.sqrt(objPos[0]**2 + objPos[1]**2), PRECISION)
+
+                        if tDist > ik.S_LEN + ik.E_LEN:
+                            self.body.move(self.CLOSE_MOVE_TIME, Direction.FORWARD)
+                            print('too far')
+                        elif tDist < ik.E_LEN - ik.S_LEN:
+                            self.body.move(self.CLOSE_MOVE_TIME, Direction.BACKWARD)
+                            print('too close')
+                        else:
+                            shoulderAngle, elbowAngle = ik.calcAngles(objPos)
+                            if (Arm.SHOULDER_MIN_DOM <= shoulderAngle <= Arm.SHOULDER_MAX_DOM) and (Arm.ELBOW_MIN_DOM <= elbowAngle <= Arm.ELBOW_MAX_DOM):
+                                return True
+                            else:
+                                self.body.move(self.CLOSE_MOVE_TIME, Direction.FORWARD)
+                                print('dunno, move closer')
+
+                else:
+                    print('lost')
+                    return False
+
+                # readjust the camera
+                time.sleep(self.CLOSE_REF_TIME)
                 rawCapture.truncate(0)
